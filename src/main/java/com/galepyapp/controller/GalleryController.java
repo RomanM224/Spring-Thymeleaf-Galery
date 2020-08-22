cac15679760ab3dac7e6cf726d48d4bc2c092d28
Roman <romanmaistruk@gmail.com>
2020-08-22 17:57:50 +0200

    first commit

---
+++ src/main/java/com/galepyapp/controller/GalleryController.java
@@ -0,0 +1,15 @@
+package com.galepyapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GalleryController {
+    
+    @RequestMapping(value = "/")
+    public String listOfImages() {
+        return "home";
+    }
+
+}
